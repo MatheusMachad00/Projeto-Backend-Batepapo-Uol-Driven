a904b7cb38d5697155fd38271205cf5252b2db77
MatheusMachad00 <matheus_m_machado@hotmail.com>
2022-06-22 10:43:44 -0300

    Feat: Added database connection for participant route

--- src/index.js
+++ src/index.js
@@ -1,15 +1,30 @@
 import express from 'express';
 import cors from 'cors';
+import { MongoClient } from "mongodb";
+import dotenv from 'dotenv';
+dotenv.config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
+const mongoClient = new MongoClient(process.env.URL_MONGO);
+let db;
 
+mongoClient.connect().then(() => {
+	db = mongoClient.db("chat_uol");
+});
 
-app.get("/", (_, res) => {
+app.get("/participants", (_, res) => {
+	db.collection("participants").find().toArray().then(participants => {
+		res.send(participants);
+	});
+});
 
-    res.status(200).send('teste');
+app.post("/participants", (req, res) => {
+	db.collection("participants").insertOne(req.body).then(() => {
+		res.sendStatus(201);
+	});
 });
 
 app.post("/participants", (req, res) => {
