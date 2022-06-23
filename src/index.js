9c58b8c31295dacb5f6ea72ad7cce9e7e9b0b805
MatheusMachad00 <matheus_m_machado@hotmail.com>
2022-06-23 11:24:51 -0300

    Fix: Minor improvement to participants routes

--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import express from 'express';
 import cors from 'cors';
 import { MongoClient } from "mongodb";
 import dotenv from 'dotenv';
-dotenv.config();
+import dayjs from "dayjs";
+import joi from "joi";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+dotenv.config();
 
 const mongoClient = new MongoClient(process.env.URL_MONGO);
 let db;
@@ -15,38 +17,81 @@ mongoClient.connect().then(() => {
 	db = mongoClient.db("chat_uol");
 });
 
-app.get("/participants", (_, res) => {
-	db.collection("participants").find().toArray().then(participants => {
+app.get("/participants", async (_, res) => {
+
+	try {
+		const participants = await db.collection("participants").find({}).toArray();
 		res.send(participants);
-	});
+	} catch (error) {
+		res.status(500).send(error);
+	}
 });
 
-app.post("/participants", (req, res) => {
-	db.collection("participants").insertOne(req.body).then(() => {
+app.post("/participants", async (req, res) => {
+
+	try {
+		const { name } = req.body;
+
+		const userSchema = joi.object({
+			name: joi.string().min(1).required(),
+		});
+
+		const nameValidation = userSchema.validate(req.body);
+		if (nameValidation.error) {
+			res.sendStatus(422);
+			return;
+		}
+
+		const nameDuplicated = await db.collection("participants").findOne({ name: name });
+		if (nameDuplicated) {
+			res.sendStatus(409);
+			return;
+		}
+
+		await db.collection("participants").insertOne({
+			name,
+			lastStatus: Date.now(),
+		})
+
+		/* await database.collection("messages").insertOne({
+			from: name,
+			to: "Todos",
+			text: "entra na sala...",
+			type: "status",
+			time: dayjs().locale("pt-br").format("HH:mm:ss"),
+		}); */
+
+		res.sendStatus(201);
+
+	} catch (error) {
+		res.status(500).send(error);
+	}
+
+
+
+});
+
+app.get("/messages", async (_, res) => {
+
+	try {
+		const messages = await db.collection("messages").find({}).toArray();
+		res.send(messages);
+	} catch (error) {
+		res.status(500).send(error);
+	}
+});
+
+app.post("/messages", (req, res) => {
+
+
+
+	db.collection("messages").insertOne(req.body).then(() => {
 		res.sendStatus(201);
 	});
 });
 
-app.post("/participants", (req, res) => {
-    
-});
-
-
-app.get("/participants", (req, res) => {
-    
-});
-
-
-app.post("/messages", (req, res) => {
-    
-});
-
-app.get("/messages", (req, res) => {
-    
-});
-
 app.post("/status", (req, res) => {
-    
+
 });
 
 
